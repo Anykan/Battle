438d87cb6df67c2ef5e8504580fb82a6f0af00ae
Anykan <aq-22@web.de>
2018-04-25 02:30:26 +0200

    0.0.1 24.04.18

---
+++ Assets/Scripts/gameConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class gameConfig : MonoBehaviour {
+
+
+
+	void Start () {
+
+	}
+	
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/playerSetup.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class playerSetup : NetworkBehaviour {
+    
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+    Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            //deaktiviert die Startcamera beim join
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+            
+        }
+
+    }
+
+    //aktiviert die scenevcamera bei disconnecten
+    void OnDisable()
+    {
+        sceneCamera = Camera.main;
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
